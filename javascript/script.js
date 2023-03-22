3bc674e978455deddcee64d0b05d9b5740b11d5c
giovannilampis <giolampis@hotmail.com>
2023-03-22 15:58:09 +0100

    add basic conntent to script.js

--- javascript/script.js
+++ javascript/script.js
@@ -14,10 +14,40 @@ const buttonTicket = document.getElementById("button-ticket");
 
 
 buttonTicket.addEventListener("click", function() {
-    const year = birthYear.value;
-    console.log(year);
+    const birth = parseInt(birthYear.value);
 
-    const tripKm = kiloM.value;
-    console.log(tripKm);
+    // console.log(birth);
+
+    let today = new Date();
+
+    let currentYear = today.getFullYear();
+
+    // console.log(currentYear);
+
+    let age = currentYear - birth;
+
+    // console.log(age);
+
+    const tripKm = parseInt(kiloM.value);
+
+    // console.log(tripKm);
+
+    let priceRegular = tripKm * 0.21;
+
+    let priceUnderAge = priceRegular / 100 * 80;
+
+    let priceRetired = priceRegular / 100 * 60;
+
+    let responcePrice = document.getElementById("responce");
+
+    if (age < 18) {
+        responcePrice.innerHTML = priceUnderAge;
+    } else if (age > 64) {
+        responcePrice.innerHTML = priceRetired;
+    } else {
+        responcePrice.innerHTML = priceRegular
+    }
+
+    event.preventDefault();
 
 })
